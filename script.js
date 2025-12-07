a1db54400686f54d566d15d11c3104a2b352fc1e
Kevin <kevinkahara39@gmail.com>
2025-12-08 01:41:01 +0300

    Ready to be pushed to prod but after some few styling adjustments

--- script.js
+++ script.js
@@ -14,6 +14,44 @@ const square = document.querySelectorAll('.square');
 
 square.forEach((square) => {
     square.addEventListener('mouseover', () => {
-        square.style.backgroundColor = 'red','green','blue';
+        square.style.backgroundColor = 'red';
     });
+});
+
+const button = document.querySelector('#btn');
+
+button.addEventListener('click', () => {
+    let message = prompt('How many Grid Squares do you want?');
+    let num = parseInt(message);
+
+    if(isNaN(num)) {
+        alert('Invalid input.Please input numbers');
+    }
+    else if(num < 1 || num > 100) {
+        alert('Please input numbers between 1 and 100');
+    }
+    else {
+        alert(num);
+    }
+
+    container.textContent = '';
+    let newSquares = num * num;
+
+    for(let i = 0; i < newSquares; i++) {
+        const gridItem = document.createElement('div');
+        gridItem.classList.add('gridItem','square');
+        container.appendChild(gridItem);
+        let sizeItem = container.clientWidth / num;
+        gridItem.style.width = `${sizeItem}px`;
+        gridItem.style.height = `${sizeItem}px`;
+    }
+
+    const newSquaresElements = document.querySelectorAll('.square');
+    newSquaresElements.forEach((square) => {
+        square.addEventListener('mouseover', () => {
+            square.style.backgroundColor = 'red';
+        });
+
+    });
+
 });
